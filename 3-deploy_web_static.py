6abfd47f9ea64e1af07848625b21ed31cdd89ea8
root <root@DESKTOP-1N3TEFC>
2024-05-04 01:56:42 +0300

    3-deploy_web_static.py

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -42,7 +42,7 @@ def do_deploy(archive_path):
         run('ln -s {} /data/web_static/current'.format(no_tar))
         print('New version deployed!')
         return True
-    except:
+    except Exception:
         return False
 
 
