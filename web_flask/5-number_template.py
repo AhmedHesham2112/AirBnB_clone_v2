0f75071c34ec142725d2c11270592f16cbb61e46
root <root@DESKTOP-1N3TEFC>
2024-05-18 00:44:13 +0300

    number template

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -11,7 +11,7 @@ Routes:
     /number/<n>: display “n is a number” only if n is an integer
 you must use the option strict_slashes=False in your route definition
 """
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -48,6 +48,12 @@ def python_is_cool(text="is cool"):
 def number_route(n):
     """display 'n is a number' only if n is an integer"""
     return "{} is a number".format(n)
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def number_template(n):
+    """display a HTML page only if n is an integer"""
+    return render_template('5-number.html', n=n)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
