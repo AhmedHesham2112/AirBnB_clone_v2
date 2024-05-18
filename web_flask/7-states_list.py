0aadc7fa3afdbf1019fa642b94c1b4d0a8ec0fc7
root <root@DESKTOP-1N3TEFC>
2024-05-18 18:01:03 +0300

    states list

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Starts a Flask web application.
+
+our web application must be listening on 0.0.0.0, port 5000
+You must use storage for fetching data from the storage engine
+(FileStorage or DBStorage) => from models import storage and storage.all(...)
+After each request you must remove the current SQLAlchemy Session:
+Declare a method to handle @app.teardown_appcontext
+Call in this method storage.close()
+Routes:
+/states_list: display a HTML page: (inside the tag BODY)
+H1 tag: “States”
+UL tag: with the list of all State objects present in DBStorage
+sorted by name (A->Z) tip
+LI tag: description of one State: <state.id>: <B><state.name></B>
+Import this 7-dump to have some data
+You must use the option strict_slashes=False in your route definition
+"""
+
+from models import storage
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    """display a HTML page: (inside the tag BODY) H1 tag: “States"""
+    states = storage.all("State")
+    return render_template("7-states_list.html", states=states)
+
+@app.teardown_appcontext
+def teardown(exc):
+    """Remove the current SQLAlchemy session."""
+    storage.close()
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
